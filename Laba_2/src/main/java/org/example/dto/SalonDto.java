adb1c787cea36791bca54f457b6cea01404af683
parviz <parvizumarov78@gmail.com>
2024-10-06 13:35:52 +0500

    start doing laba 2

---
+++ Laba_2/src/main/java/org/example/dto/SalonDto.java
@@ -0,0 +1,13 @@
+package org.example.dto;
+
+import lombok.Data;
+
+@Data
+public class SalonDto {
+    private Integer id;
+    private String address;
+    private String images;
+    private String longitude;
+    private String latitude;
+
+}
---
+++ Laba_2/src/main/java/org/example/dto/TokenDto.java
@@ -0,0 +1,10 @@
+package org.example.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenDto {
+    private int uid;
+    private String mail;
+    private String token;
+}

8a3ae1a3b7cbd9b4092002429b90ce4f7d83fc55
parviz <parvizumarov78@gmail.com>
2024-11-05 14:38:01 +0500

    delete ClassInfoWriter

--- Laba_5/src/main/java/org/example/command/Command.java
+++ Laba_5/src/main/java/org/example/command/Command.java
@@ -8,6 +8,9 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+/**
+ * @author parviz
+ */
 @ShellComponent
 @RequiredArgsConstructor
 public class Command {
@@ -16,31 +19,60 @@ public class Command {
     private final LanguageService languageService;
     private final ServiceCRUD serviceCRUD;
 
+    /**
+     * command lang that get current language
+     * @return current selected language
+     */
     @ShellMethod(key = "lang")
     public String getCurrentLanguage() {
         return languageService.getCurrentLanguage();
     }
 
+    /**
+     * Shell command lang -c for change language
+     * @param arg value that we want to change
+     * @return changed language
+     */
     @ShellMethod(key = "lang -c")
     public String changeLanguage(@ShellOption String arg) {
         return languageService.setCurrentLanguage(arg);
     }
 
+
+    /**
+     * Shell command find -a for get all barbers from db
+     * @return list of Barber
+     */
     @ShellMethod(key = "find -a")
     public String findAll() {
         return barberService.getAll();
     }
 
+    /**
+     * Shell find command find barber by argument
+     * @param arg the barber name
+     * @return entity of Barber
+     */
     @ShellMethod(key = "find")
     public String find(@ShellOption String arg) {
         return barberService.get(arg);
     }
 
+    /**
+     * Shell command barber with -d flag delete barber
+     * @param arg name of barber where you want delete
+     * @return message that delete is completed or not
+     */
     @ShellMethod(key = "barber -d")
     public String delete(@ShellOption String arg) {
         return barberService.delete(arg);
     }
 
+    /**
+     * Shell command barber -u update barber info
+     * @param arg name of barber
+     * @return message
+     */
     @ShellMethod(key = "barber -u")
     public String update(@ShellOption String arg) {
         return barberService.update(arg);
